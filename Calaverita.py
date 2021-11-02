b8dbcf3cb5578c6f776b075e36646d5a0dcc9ad1
Aiac5 <ayax.valencia@digitalfamily.mx>
2021-11-01 18:28:00 -0600

    Ayax

---
+++ Calaverita.py
@@ -0,0 +1,21 @@
+def handler(event, context):
+    print('hello world')
+    return {"message": "Successfully executed"}
+
+class ChineseAnimalIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("ChineseAnimalIntent")(handler_input)
+
+    def handle(self, handler_input):
+        year = handler_input.request_envelope.request.intent.slots['year'].value
+        speech_text = "My custom Intent handler"
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response 
+
+sb = SkillBuilder()
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+sb.add_request_handler(ChineseAnimalIntentHandler())
+
+def handler(event, context):
+    return sb.lambda_handler()(event, context)
