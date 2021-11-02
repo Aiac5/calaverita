bf9e3ba67ebe1fcd2c1c6095316887dbee777b54
Aiac5 <ayax.valencia@digitalfamily.mx>
2021-11-01 19:46:29 -0600

    adgsdg

--- Calaverita.py
+++ Calaverita.py
@@ -8,14 +8,17 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        handler_input.response_builder.speak("Hola a todos, esta es la calaverita que queremos compartir con ustedes"+
+        handler_input.response_builder.speak("Hola a todos, esta es la calaverita que queremos compartir con ustedes."+
                                             'Estaba Romi muy emocionada,'+
                                             'por transformar el futuro de la educación,'+
-                                            'con el learning hub se sentia muy preparada,'+
+                                            'con el learning jub se sentia muy preparada,'+
                                             'pero nunca imagino afrontarse con tal aberración,'+
-                                            'ante sus una huesuda muy arreglada,'+
-                                            'llego por ella sin mas antelación'+
-                                            'ja, ja, ja').set_should_end_session(False)
+                                            'ante sus ojos una huesuda muy arreglada,'+
+                                            'llego por ella sin mas antelación.'+
+                                            'ja, ja, ja.'+
+                                            'Any se encontraba en el chai comiendo'+
+                                            'despues de un exhausto día de trabajo'+
+                                            'cuando una extraña figura le estaba sonriendo').set_should_end_session(False)
         return handler_input.response_builder.response    
 
 class CatchAllExceptionHandler(AbstractExceptionHandler):
